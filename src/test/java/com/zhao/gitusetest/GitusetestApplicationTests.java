398afad1a79b1c4719b109172fbb6987b63872ff
Noblegasesgoo <z2632816823@outlook.com>
2021-03-05 15:27:44 +0800

    idea尝试第一次git集成提交测试

---
+++ src/test/java/com/zhao/gitusetest/GitusetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhao.gitusetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitusetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
